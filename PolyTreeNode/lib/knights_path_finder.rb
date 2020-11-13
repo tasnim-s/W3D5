dcb61d386ad0ab52727a5cf56c2b680d1a63db1d
tsaiduzzaman <tasnim.s@nyu.edu>
2020-11-13 18:11:58 -0500

    Started BFS Tree

--- PolyTreeNode/lib/knights_path_finder.rb
+++ PolyTreeNode/lib/knights_path_finder.rb
@@ -3,31 +3,45 @@ require_relative "00_tree_node.rb"
 class KnightPathFinder
 
     def self.valid_moves(pos)
-        board = Array.new(8) {Array.new(8)}
         combos = []
-        avail_long = [-3, 3]
+        avail_long = [-2, 2]
         avail_short = [-1, 1]
+
         combos += (avail_long.product(avail_short) + avail_short.product(avail_long))
-        combos.map do |combo|
+
+        combos.map! do |combo|
             [pos[0] + combo[0] , pos[1] + combo[1]]
         end
+
+        combos.reject {|combo| combo[0] < 0 || combo[0] > 7 || combo[1] < 0 || combo[1] > 7}
     end
 
     def initialize(start_pos)
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
-        self.build_move_tree
+        build_move_tree
     end
 
     def build_move_tree
-        self.root_node.bfs()
+        queue = [@root_node]
+
+        until queue.empty?
+            checking = queue.shift
+            new_move_positions(@root_node.value)
+        end
+
+        []
     end
 
     def new_move_positions(pos)
         remaining = KnightPathFinder.valid_moves(pos).reject {|move| @considered_positions.include?(move)}
-        @considered_positions += considered_positions
+        @considered_positions += remaining
     end
 
 
 end
+
+
+p kpf = KnightPathFinder.new([0, 0])
+p kpf.new_move_positions([2,1])
